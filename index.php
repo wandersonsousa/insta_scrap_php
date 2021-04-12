cb5498d6bc5a511b6fba40ff2a9fdc299309976b
Wanderson Sousa <wandersonsousa3004@gmail.com>
2021-04-12 19:39:51 -0300

    host my project

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+require_once './vendor/autoload.php';
+
+use Goutte\Client;
+
+$client = new Client();
+
+$crawler = $client->request('GET', 'https://www.instagram.com/wander_dev/');
+
+echo $crawler->filterXpath('//meta[@property="og:image"]')->attr('content');
+
+echo $crawler->html('<b>Error</b>');
