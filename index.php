08b8e9a8bfe6a1a8858b97e6f5693de10ad79242
Wanderson Sousa <wandersonsousa3004@gmail.com>
2021-04-12 20:16:40 -0300

    update data

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ $crawler = $client->request('GET', 'https://www.instagram.com/wander_dev/');
 
 //echo $crawler->filterXpath('//meta[@property="og:image"]')->attr('content');
 
-echo json_encode([
+header('Content-Type: application/json');
+$data = [
     "data"=> $crawler->html('<b>Error</b>')
-]);
+];
+
+echo json_encode($data);
